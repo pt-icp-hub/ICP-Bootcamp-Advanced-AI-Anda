ce2d0e1586a799a10193d38f6b291cf7154cb924
espieux <148326182+espieux@users.noreply.github.com>
2025-02-20 13:51:52 +0000

    update setup script to install additional dependencies for development

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -3,3 +3,5 @@
 dfx identity new codespace_dev --storage-mode=plaintext
 dfx identity use codespace_dev
 dfx start --background --clean
+apt update && apt install -y pkg-config libssl-dev
+apt-get update && apt-get install -y protobuf-compiler
