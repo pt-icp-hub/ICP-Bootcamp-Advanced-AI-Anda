59c4131165584a83d947289e15ac05ced48d95ab
espieux <148326182+espieux@users.noreply.github.com>
2025-02-20 15:13:37 +0100

    Update setup.sh

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -3,6 +3,6 @@
 dfx identity new codespace_dev --storage-mode=plaintext
 dfx identity use codespace_dev
 dfx start --background --clean
-apt update && apt install -y pkg-config libssl-dev build-essential
+apt update && apt install -y pkg-config libssl-dev build-essential clang
 apt-get update && apt-get install -y protobuf-compiler
 rustup target add wasm32-wasi
